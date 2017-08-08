41df62c8bf6e036d88441e87d5916939708d2bcd
Vitalik <makvitalik@gmail.com>
2017-08-08 14:15:13 +0300

    Added required methods from implemented protocols and some functions like initAudio and parsePokemonCSV file as well

--- Pokeedex/ViewController.swift
+++ Pokeedex/ViewController.swift
@@ -50,7 +50,121 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text == nil || searchBar.text == "" {
+        
+            inSearchMode = false
+            collection.reloadData()
+            view.endEditing(true)
+        
+        } else {
+        
+            inSearchMode = true
+            
+            let lowerChar = searchBar.text!.lowercased()
+            
+            filteredPokemon = pokemon.filter({ $0.name.range(of: lowerChar) != nil })
+            collection.reloadData()
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        view.endEditing(true)
+    }
 
+    func initAudio() {
+    
+        let path = Bundle.main.path(forResource: "PokeMusic", ofType: "mp3")!
+        
+        do {
+            
+            musicPleyer = try AVAudioPlayer(contentsOf: URL(string: path)!)
+            musicPleyer.prepareToPlay()
+            musicPleyer.numberOfLoops = -1
+            musicPleyer.play()
+            
+        } catch let err as NSError {
+            
+            print("There some error in playing music \(err.debugDescription)")
+        }
+    }
+    
+    func parsePokemonCSV() {
+        
+        let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
+        
+        do {
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows
+            print(rows)
+            
+            for row in rows {
+            
+                let pokeId = Int(row["id"]!)!
+                let name = row["identifier"]!
+                
+                let addNewPokemon = Pokemon(name: name, pokedexId: pokeId)
+                pokemon.append(addNewPokemon)
+                
+            }
+            
+        } catch let err as NSError {
+            print("There's some error:\(err.debugDescription)")
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokemonCell", for: indexPath) as? PokemonCell {
+            
+            let poke: Pokemon!
+            
+            if inSearchMode {
+            
+                poke = filteredPokemon[indexPath.row]
+                cell.configureCell(poke)
+            
+            } else {
+            
+                poke = pokemon[indexPath.row]
+                
+                cell.configureCell(poke)
+            }
+            
+            return cell
+            
+        } else {
+        
+            return UICollectionViewCell()
+        }
+    }
 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        if inSearchMode {
+            
+            return filteredPokemon.count
+        
+        } else {
+        
+            return pokemon.count
+        }
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 90, height: 87)
+    }
 }
 
