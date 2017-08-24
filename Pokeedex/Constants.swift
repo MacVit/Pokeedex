7478c9f6c724823053df9537df9450b138614058
Vitalik Maksymliuk <makvitalik@gmail.com>
2017-08-24 13:54:00 +0300

    Added some URL constant to communicate with Pokedex API

---
+++ Pokeedex/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Pokeedex
+//
+//  Created by Vitaliy Maksymlyuk on 8/21/17.
+//  Copyright © 2017 Vitalik Maksymlyuk. All rights reserved.
+//
+
+import Foundation
+
+
+let URL_BASE = "http://pokeapi.co"
+
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
